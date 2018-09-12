98d89718e6cdf8c293a7203fe0b8d2cd19c2525b
Will Miller <willmiller@Wills-MBP.attlocal.net>
2018-09-12 16:23:20 -0400

    heroku auth configuration

--- client/src/components/auth/authVariables.js
+++ client/src/components/auth/authVariables.js
@@ -1,5 +1,5 @@
 export const Auth_Configure = {
     domain: 'coffeefinder.auth0.com',
     clientId: 'eMpLssdovFz0Q0JgYvHSfUka7m7eq390',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://pure-taiga-14555.herokuapp.com/callback'
 };
